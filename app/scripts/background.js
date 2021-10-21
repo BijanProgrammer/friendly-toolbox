2e2be2fd2d602a830c24e79c735f673994c67dea
BijanProgrammer <BijanEisapour@gmail.com>
2021-10-21 22:29:11 +0330

    [ADD] shortcuts

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,3 +1,13 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Hello, friend!');
 });
+
+chrome.commands.onCommand.addListener(command => {
+    console.log('command:', command);
+
+    if (!command.startsWith('run-')) return;
+
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, {methodName: command.substr(4)}, () => {});
+    });
+});
