2e3480212c0505a26dd9d6aaf11f7b54ff66777b
BijanProgrammer <BijanEisapour@gmail.com>
2024-04-06 16:32:04 +0330

    [ADD] google sorry bypass

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -11,3 +11,24 @@ chrome.commands.onCommand.addListener((command) => {
         chrome.tabs.sendMessage(tabs[0].id, {methodName: command.substr(4)}, () => {});
     });
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (!tab.incognito || !changeInfo.url || !changeInfo.url.startsWith('https://www.google.com/sorry')) {
+        return;
+    }
+
+    const originalUrl = new URL(changeInfo.url).searchParams.get('continue');
+    const query = new URL(originalUrl).searchParams.get('q');
+    const updatedUrl = `https://duckduckgo.com/?q=${query}`;
+
+    chrome.scripting
+        .executeScript({
+            target: {tabId},
+            func: (updatedUrl) => {
+                location.href = updatedUrl;
+            },
+            args: [updatedUrl],
+            injectImmediately: true,
+        })
+        .then();
+});
