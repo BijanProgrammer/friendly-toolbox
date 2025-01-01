467e6f43dd828c8c4d9c49d5468ea7c44289428c
BijanProgrammer <BijanEisapour@gmail.com>
2025-01-01 08:38:29 +0330

    [UPDATE] gov - work hard - message

--- app/scripts/tools/gov/work-hard.js
+++ app/scripts/tools/gov/work-hard.js
@@ -6,12 +6,34 @@ const sumUpDays = () => {
     return days.reduce((sum, x) => sum + x, 0);
 };
 
-const govWorkHard = () => {
-    const sum = sumUpDays();
-    const content = `Total Days: ${sum}`;
+const calculateDays = (days) => {
+    return `${days} Days`;
+};
+
+const calculateMonths = (days) => {
+    const months = Math.floor(days / 30);
+    const remainingDays = days % 30;
+
+    return `${months} Months and ${calculateDays(remainingDays)}`;
+};
+
+const calculateYears = (days) => {
+    const years = Math.floor(days / 365);
+    const remainingDays = days % 365;
+
+    return `${years} Years and ${calculateMonths(remainingDays)}`;
+};
+
+const show = (days) => {
+    const content = [calculateDays(days), calculateMonths(days), calculateYears(days)].join('\n');
 
     console.log(content);
     alert(content);
+};
+
+const govWorkHard = () => {
+    const days = sumUpDays();
+    show(days);
 };
 
 tools['govWorkHard'] = govWorkHard;
